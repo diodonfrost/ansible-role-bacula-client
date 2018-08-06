6e6115af5a991162dc15b549db84271ccd99f6ab
diodonfrost <antoine.mace@protonmail.com>
2018-08-06 21:21:07 +0200

    remove debian-7 test and set bacula-fd.conf directory

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -8,3 +8,13 @@ control 'install-01' do
     it { should be_installed }
   end
 end
+
+control 'install-02' do
+  impact 1.0
+  title 'Bacula-client service'
+  desc 'Bacula-client service should be started and enable'
+  describe service('bacula-client') do
+    it { should be_running }
+    it { should be_enable }
+  end
+end
